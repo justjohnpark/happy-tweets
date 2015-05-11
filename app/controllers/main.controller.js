207b6b8d809c72f19cb384be6b493c7e4bfd544c
John Park <just.johnpark@gmail.com>
2015-05-11 14:26:16 -0400

    fine-tuned timers

--- app/controllers/main.controller.js
+++ app/controllers/main.controller.js
@@ -5,50 +5,54 @@
 
   function mainController($scope, $timeout, TwitterFactory) {
     vm = this;
-    vm.coordinates = [];
-    vm.markers = [];
+    vm.coordinates = [[],[]];
+    vm.markers = [[],[]];
+    vm.initiate = initiate;
     vm.extractCoordinates = extractCoordinates;
     vm.addMarker = addMarker;
-    // vm.emptyTheNest = emptyTheNest;
     vm.map;
 
     $scope.$on('mapInitialized', function(event, evtMap) {
       vm.map = evtMap;  
     }, 1000);
 
-    // while (vm.coordinates !== []) {
+    vm.initiate(0);
+
+    function initiate(set) {      
       TwitterFactory.getTweets().then(function() {
         TwitterFactory.splitTweets();
-        vm.extractCoordinates();
-        vm.addMarker();
+        vm.extractCoordinates(set);
+        vm.addMarker(set);
       });
-    // }
+    }
 
-    function extractCoordinates() {
+    function extractCoordinates(set) {
       for (coordinates in TwitterFactory.processed) {
         var first = Number(coordinates.split(',')[0]);
         var second = Number(coordinates.split(',')[1]);
-        vm.coordinates.push(new google.maps.LatLng(first, second));
+        vm.coordinates[set].push(new google.maps.LatLng(first, second));
       }
       console.log(vm.coordinates);
     }
 
     var iterator = 0;  
     var counter = 0;
-    function addMarker() {
-      if (vm.map !== undefined) {
-        for (var i=0; i<vm.coordinates.length; i++) {
 
+    function addMarker(set) {
+      iterator = 0;
+      counter = 0;
+      if (vm.map !== undefined) {
+        for (var i=0; i<vm.coordinates[set].length; i++) {
           $timeout(function() {
             // add a marker this way does not sync. marker with <marker> tag
             var marker = new google.maps.Marker({
-              position: vm.coordinates[iterator++],
+              position: vm.coordinates[set][iterator++],
               map: vm.map,
               icon: "../images/smile.png",
               draggable: false,
               animation: google.maps.Animation.DROP
             });
-            vm.markers.push(marker);
+            vm.markers[set].push(marker);
           }, i * 2000);
         }
       } else {
@@ -57,14 +61,23 @@
         }), 500);
       }
       $timeout(function() {
-        // console.log(vm.markers);
-        for (var j=0; j<vm.markers.length; j++) {
+        for (var j=0; j<vm.coordinates[set].length; j++) {
           $timeout(function() {
-            vm.markers[counter++].setMap(null);
+            if (counter === (vm.coordinates[set].length-1)) {
+              vm.markers[set][counter].setMap(null);
+              vm.coordinates[set] = [];
+              vm.markers[set] = [];
+              if (set === 0) {
+                vm.initiate(1);                
+              } else {
+                vm.initiate(0);
+              }
+            } else {
+              vm.markers[set][counter++].setMap(null); 
+            }
           }, j * 2000);
         }
-        // console.log(vm.markers);
-      }, vm.coordinates.length * 2000);
+      }, 8000);
     }
   }
 })();
