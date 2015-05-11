18b749070a9a674913bfdf90abba8e9ce97b046f
John Park <just.johnpark@gmail.com>
2015-05-11 16:15:55 -0400

    text shows up

--- app/controllers/main.controller.js
+++ app/controllers/main.controller.js
@@ -6,11 +6,13 @@
   function mainController($scope, $timeout, TwitterFactory) {
     vm = this;
     vm.coordinates = [[],[]];
+    vm.tweetText = [[],[]];
     vm.markers = [[],[]];
     vm.initiate = initiate;
     vm.extractCoordinates = extractCoordinates;
     vm.addMarker = addMarker;
     vm.map;
+    vm.output = "";
 
     $scope.$on('mapInitialized', function(event, evtMap) {
       vm.map = evtMap;  
@@ -34,20 +36,22 @@
         var first = Number(coordinates.split(',')[0]);
         var second = Number(coordinates.split(',')[1]);
         vm.coordinates[set].push(new google.maps.LatLng(first, second));
+        vm.tweetText[set].push(TwitterFactory.processed[set][coordinates]);
       }
       console.log(vm.coordinates[set]);
-    }
+      // console.log(vm.tweetText[set]);
+    } 
 
     var iterator = [[0],[0]];  
     var counter = [[0],[0]];
 
     function addMarker(set) {
+      vm.output = "";
       iterator[set] = 0;
       counter[set] = 0;
       if (vm.map !== undefined) {
         for (var i=0; i<vm.coordinates[set].length; i++) {
           $timeout(function() {
-            // add a marker this way does not sync. marker with <marker> tag
             var marker = new google.maps.Marker({
               position: vm.coordinates[set][iterator[set]++],
               map: vm.map,
@@ -56,6 +60,7 @@
               animation: google.maps.Animation.DROP
             });
             vm.markers[set].push(marker);
+            vm.output = vm.tweetText[set][iterator[set]];
           }, i * 2000);
         }
       } else {
--- todo.md
+++ todo.md
@@ -41,3 +41,8 @@ https://snazzymaps.com/style/14/vintage
 - https://developers.google.com/maps/tutorials/customizing/custom-markers
 - https://developers.google.com/maps/tutorials/customizing/styling-the-base-map
 - https://developers.google.com/maps/documentation/javascript/examples/icon-complex
+
+
+--one of the big issues--
+https://github.com/allenhwkim/angularjs-google-maps/issues/33
+https://github.com/allenhwkim/angularjs-google-maps/blob/master/testapp/marker_with_parent_controller.html
